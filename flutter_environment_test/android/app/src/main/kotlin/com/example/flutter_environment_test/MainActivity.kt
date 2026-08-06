742cfbb6c9af049a46cd2bef77506161d3faf2d3
walter <houisaac046@gmail.com>
2026-08-06 10:57:10 +0800

    Deleted Test Files

--- flutter_environment_test/android/app/src/main/kotlin/com/example/flutter_environment_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_environment_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_environment_test/temple_procession_tracker_app/android/app/src/main/kotlin/com/walter/temple_procession_tracker_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.walter.temple_procession_tracker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
